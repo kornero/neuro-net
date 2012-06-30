0673a17da0e446f249a0358442f8edea502e328f
Kornero <korneropro@gmail.com>
2012-06-30 22:40:19 +0400

    Init

--- src/com/neuronet/Main.java
+++
@@ -1,4 +0,0 @@
-package com.neuronet;
-
-public class Main {
-}
---
+++ src/com/neuronet/NeuroNet.java
@@ -0,0 +1,13 @@
+package com.neuronet;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class NeuroNet {
+
+    private static final Logger logger = LoggerFactory.getLogger(NeuroNet.class);
+
+    public static void main(String... args) {
+        logger.info("NeuroNet is starting.");
+    }
+}
